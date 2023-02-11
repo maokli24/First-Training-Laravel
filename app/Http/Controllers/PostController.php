dd597b4b1007c37e45cf5917a3c715b04c9c7aa7
maokli24 <102806508+maokli24@users.noreply.github.com>
2023-02-11 13:59:34 +0100

    Fixing the names like  the Laravel Documentation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class PostController extends Controller
 {
-    public function TestAction(){
+    public function index(){
         $allPosts =[
             ['id'=>1,'title'=>'MVC Pattern','posted_by'=>'Ahmed','created_at'=>'2020-04-03'],
             ['id'=>2,'title'=>'Artisan Commands','posted_by'=>'Mohammed','created_at'=>'2020-04-06'],
@@ -14,6 +14,6 @@ class TestController extends Controller
             ['id'=>4,'title'=>'Architecture','posted_by'=>'Ibrahim','created_at'=>'2020-04-28']
            
         ];
-        return view('test',['posts'=>$allPosts]);
+        return view('index',['posts'=>$allPosts]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test','App\Http\Controllers\TestController@TestAction');
+Route::get('/Posts','App\Http\Controllers\PostController@index');
